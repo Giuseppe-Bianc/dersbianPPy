038b78ba243a746f2fa53e14bb8441aac8632516
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2023-02-06 13:16:26 +0100

    input

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    print("dersbian 1.0")
+    while True:
+        line = input(">")
+        if line == "exit":
+            break
+
+        print(line)
+
+
+if __name__ == '__main__':
+    main()
